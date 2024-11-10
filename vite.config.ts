e6c1b47e770edf4eb954bee6e88f02031b62955c
Oein <oein0219@gmail.com>
2024-11-10 23:48:50 +0900

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,6 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import { imageToWebpPlugin } from "vite-plugin-image-to-webp";
 
 export default defineConfig({
-  plugins: [
-    sveltekit(),
-    imageToWebpPlugin({
-      webpQuality: {
-        quality: 90,
-        alphaQuality: 90,
-      },
-      destinationFolder: "build",
-    }),
-  ],
+  plugins: [sveltekit()],
 });
