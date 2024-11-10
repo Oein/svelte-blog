b738b05a8d3ab9e6fc284435c7d9bec518920ec9
Oein <oein0219@gmail.com>
2024-11-10 22:19:58 +0900

    revert

--- src/lib/svelte-notion-x/components/Image.svelte
+++ src/lib/svelte-notion-x/components/Image.svelte
@@ -4,12 +4,59 @@
   export let block: {
     [key: string]: any;
   };
-  const toNotionImageUrl = (url: string, blockId: string) =>
-    `/api/image${
-      url.startsWith("/image")
-        ? url
-        : `/image/${encodeURIComponent(url)}?table=block&id=${blockId}`
+  const defaultMapImageUrl = (url: string | undefined): string | undefined => {
+    if (!url) {
+      return undefined;
+    }
+
+    if (url.startsWith("data:")) {
+      return url;
+    }
+
+    // more recent versions of notion don't proxy unsplash images
+    if (url.startsWith("https://images.unsplash.com")) {
+      return url;
+    }
+
+    try {
+      const u = new URL(url);
+
+      if (
+        u.pathname.startsWith("/secure.notion-static.com") &&
+        u.hostname.endsWith(".amazonaws.com")
+      ) {
+        if (
+          u.searchParams.has("X-Amz-Credential") &&
+          u.searchParams.has("X-Amz-Signature") &&
+          u.searchParams.has("X-Amz-Algorithm")
+        ) {
+          // if the URL is already signed, then use it as-is
+          return url;
+        }
+      }
+    } catch {
+      // ignore invalid urls
+    }
+
+    if (url.startsWith("/images")) {
+      url = `https://www.notion.so${url}`;
+    }
+
+    url = `https://www.notion.so${
+      url.startsWith("/image") ? url : `/image/${encodeURIComponent(url)}`
     }`;
+
+    const notionImageUrlV2 = new URL(url);
+    notionImageUrlV2.searchParams.set("id", block.id);
+    notionImageUrlV2.searchParams.set("cache", "v2");
+
+    url = notionImageUrlV2.toString();
+
+    return url;
+  };
+
+  const toNotionImageUrl = (url: string, blockId: string) =>
+    defaultMapImageUrl(url);
 </script>
 
 <img
