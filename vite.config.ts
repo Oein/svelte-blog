130950d34d690c640503645bd0b10e31159bb91b
Oein <oein0219@gmail.com>
2024-11-10 23:42:24 +0900

    webp

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { imageToWebpPlugin } from "vite-plugin-image-to-webp";
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [
+    sveltekit(),
+    imageToWebpPlugin({
+      webpQuality: {
+        quality: 90,
+        alphaQuality: 90,
+      },
+    }),
+  ],
 });
