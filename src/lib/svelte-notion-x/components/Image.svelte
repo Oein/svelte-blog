cbaf40d0ccd1b4a435ee1a9c64fba522a4f3483a
Oein <oein0219@gmail.com>
2024-11-10 22:44:27 +0900

    image test

--- src/lib/svelte-notion-x/components/Image.svelte
+++ src/lib/svelte-notion-x/components/Image.svelte
@@ -18,6 +18,38 @@
       return url;
     }
 
+    if (
+      url.startsWith("https://prod-files-secure.") &&
+      url.includes(".amazonaws.com/")
+    ) {
+      const u = new URL(url);
+      const urlspli = u.pathname.split("/");
+      const region = u.hostname
+        .replace("prod-files-secure.s3.", "")
+        .replace(".amazonaws.com", "");
+      const sp = u.searchParams;
+      const arr = [
+        region,
+        urlspli[1],
+        urlspli[2],
+        urlspli[3],
+        sp.get("X-Amz-Algorithm"),
+        sp.get("X-Amz-Content-Sha256"),
+        sp.get("X-Amz-Credential"),
+        sp.get("X-Amz-Date"),
+        sp.get("X-Amz-Expires"),
+        sp.get("X-Amz-Signature"),
+        sp.get("X-Amz-SignedHeaders"),
+        sp.get("x-id"),
+      ]
+        .filter((x) => !!x)
+        .map((x) => btoa(x!));
+      console.log(arr);
+      const ur = "/image/" + arr.join("/");
+
+      return ur;
+    }
+
     try {
       const u = new URL(url);
 
@@ -31,6 +63,8 @@
           u.searchParams.has("X-Amz-Algorithm")
         ) {
           // if the URL is already signed, then use it as-is
+
+          console.log("IMGSO1");
           return url;
         }
       }
@@ -39,8 +73,12 @@
     }
 
     if (url.startsWith("/images")) {
+      console.log("IMGSO2");
       url = `https://www.notion.so${url}`;
     }
+
+    if (url.startsWith("/image")) console.log("IMGSO3");
+    else console.log("IMGSO4");
 
     url = `https://www.notion.so${
       url.startsWith("/image") ? url : `/image/${encodeURIComponent(url)}`
